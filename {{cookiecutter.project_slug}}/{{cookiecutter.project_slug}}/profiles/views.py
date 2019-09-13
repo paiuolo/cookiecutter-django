2e267a157cf0a985f8ca6a6ba29e57c66ce2a083
pai <paiuolo@gmail.com>
2019-09-13 23:35:30 +0200

    update

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/profiles/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/profiles/views.py
@@ -1,5 +1,18 @@
+import logging
+
 from django_sso_app.core.apps.profiles.views import ProfileViewSet as DjangoSSoAppProfileViewSet
+from django_sso_app.core.utils import get_profile_model
+
+from backend.permissions import is_staff
+
+logger = logging.getLogger('profiles')
+Profile = get_profile_model()
 
 
 class ProfileViewSet(DjangoSSoAppProfileViewSet):
-    pass
+    def get_queryset(self):
+        user = self.request.user
+        if is_staff(user):
+            return Profile.objects.all()
+        else:
+            return Profile.objects.filter(user=user)
