55b76cfe87b1e118bf43bc3b908afa75d4f9ef1e
pai <paiuolo@gmail.com>
2019-12-16 14:50:56 +0100

    update

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -1,57 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
-from .forms import UserChangeForm, UserCreationForm
+from django_sso_app.core.apps.users.admin import UserAdmin
 
 User = get_user_model()
 
 
-if settings.DJANGO_SSO_APP_BACKEND_ENABLED or settings.DJANGO_SSO_APP_ENABLED:
-    from django_sso_app.core.apps.profiles.models import Profile
-
-    if settings.DJANGO_SSO_APP_BACKEND_ENABLED:
-        _list_display = ["username", "email", "is_superuser"]
-        _search_fields = ["username", "email",]
-    elif settings.DJANGO_SSO_APP_ENABLED:
-        _list_display = ["username", "email", "is_superuser"]
-        _search_fields = ["username", "email",]
-
-
-    class ProfileInline(admin.StackedInline):
-        model = Profile
-        max_num = 1
-        can_delete = False
-
-        def get_readonly_fields(self, request, obj=None):
-            # if obj is None:
-            return ['sso_id', 'sso_rev']
-
-
-    @admin.register(User)
-    class UserAdmin(auth_admin.UserAdmin):
-        form = UserChangeForm
-        add_form = UserCreationForm
-
-        # pai
-        # fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-        fieldsets = auth_admin.UserAdmin.fieldsets
-        list_display = _list_display
-        search_fields = _search_fields
-
-        # pai
-        inlines = [ProfileInline]
-
-else:
-    @admin.register(User)
-    class UserAdmin(auth_admin.UserAdmin):
-        form = UserChangeForm
-        add_form = UserCreationForm
-
-        # pai
-        # fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-        fieldsets = auth_admin.UserAdmin.fieldsets
-        list_display = ["username", "email", "is_superuser"]
-        search_fields = ["username", "email"]
-
+admin.site.register(User, UserAdmin)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,9 +1,15 @@
-from django.conf import settings
-from django.urls import reverse
+import logging
 
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from django_sso_app.core.apps.users.models import DjangoSsoAppUserModelMixin
+
+logger = logging.getLogger('django_sso_app.backend.users')
 
 
-class User(AbstractUser):
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+class User(AbstractUser, DjangoSsoAppUserModelMixin):
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
